128b922960fd776ebecccca5bafba1511f9d52cb
dharun <dharundds@gmail.com>
2021-04-11 20:11:36 +0530

    update

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-from flask import Flask,render_template,redirect,url_for,request
-from flask_socketio import SocketIO,join_room
+from flask import Flask, render_template, redirect, url_for, request
+from flask_socketio import SocketIO, join_room, leave_room, send
 app = Flask(__name__)
-socket = SocketIO(app)
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/chat')
 def chat():
-    username = request.args.get('name')
+    username = request.args.get('username')
     room = request.args.get('room')
-    if username and room :
-        return render_template('chat.html',username=username,room=room)
+    if username and room:
+        return render_template('chat.html', username=username, room=room)
     else:
         return redirect(url_for('home'))
-@socket.on('join_room') 
+
+
+@socketio.on('send_msg')
+def handle_send_message_event(data):
+    socketio.emit('receive_msg', data, room=data['room'])
+
+
+@socketio.on('leave_room')
+def leaving_room(data):
+    leave_room(data['room'])
+    socketio.emit('leave_room_announcement', data, room=data['room'])
+
+
+@socketio.on('join_room')
 def joinning_room(data):
-    #app.logger.info("{} has joined the room {}".format(data['username'],data['room']))
     join_room(data['room'])
-    socket.emit('joined_room',data)
+    socketio.emit('join_room_announcement', data, room=data['room'])
 
 
-        
-if __name__=="__main__":
-    socket.run(app,debug=True)  
-    
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
--- templates/chat.js
+++
@@ -1,8 +0,0 @@
-const socket = io.connect('http://127.0.0.1:5000/');
-
-socket.on('connect',()=>{
-    socket.emit('join-room',{
-        username: "{{username}}",
-        room:"{{room}}"
-    })
-} )
