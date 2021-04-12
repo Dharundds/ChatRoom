149883b4c7b893aa497d32be44667cfac9e68ffe
dharun <dharundds@gmail.com>
2021-04-12 10:19:01 +0530

    update with db

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_socketio import SocketIO, join_room, leave_room, send
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from db import get_user
+
+
 app = Flask(__name__)
+app.secret_key = 'Centigrade'
 socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 
 @app.route('/')
@@ -9,7 +17,34 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    message = ''
+    if request.method == "POST":
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+        user = get_user(username)
+
+        if user and user.check_password(password_input):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            message = 'Failed to login'
+
+    return render_template('login.html', message=message)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
 @app.route('/chat')
+@login_required
 def chat():
     username = request.args.get('username')
     room = request.args.get('room')
@@ -34,6 +69,11 @@ def leaving_room(data):
 def joinning_room(data):
     join_room(data['room'])
     socketio.emit('join_room_announcement', data, room=data['room'])
+
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
 
 
 if __name__ == "__main__":
---
+++ db.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+from user import User
+
+client = MongoClient(
+    "mongodb+srv://Centigrade:Centigrade@centigrade-chatroom.l4cxo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+chat_db = client.get_database("ChatDB")
+users_collections = chat_db.get_collection("users")
+
+
+def save_user(username, password):
+    password = generate_password_hash(password)
+    users_collections.insert_one({'_id': username, 'password': password})
+
+
+def get_user(username):
+    user_data = users_collections.find_one({'_id':username})
+    return User(user_data['_id'],user_data['password']) if user_data else  None
+
+
+
+if __name__=="__main__":
+    save_user("dharun", "dharun")
