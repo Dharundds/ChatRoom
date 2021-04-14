9f19837eb42e08311751d5cbcbfdbcf67308c25b
Hrithik <hrithikmj003@gmail.com>
2021-04-14 10:55:16 +0530

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,19 @@
+selectRoom = () => {
+  let room = document.getElementById("dropp").value;
+  console.log(room);
+  document.getElementById("room").value = room;
+  document.getElementById("dropdownMenuButton").innerHTML = "Admin";
+};
+Room2 = () => {
+  let room2 = document.getElementById("dropp2").value;
+  console.log(room2);
+  document.getElementById("room").value = room2;
+  document.getElementById("dropdownMenuButton").innerHTML = "Room2";
+};
 
-  selectRoom = () => {
-    let room = document.getElementById("dropp").value;
-    console.log(room);
-    document.getElementById("room").value = room;
-    document.getElementById("dropdownMenuButton").innerHTML="Admin";
-  };
-  Room2 = () => {
-    let room2 = document.getElementById("dropp2").value;
-    console.log(room2);
-    document.getElementById("room").value = room2;
-    document.getElementById("dropdownMenuButton").innerHTML="Room2";
-  };
-
-  Room3 = () => {
-    let room2 = document.getElementById("dropp3").value;
-    console.log(room2);
-    document.getElementById("room").value = room2;
-    document.getElementById("dropdownMenuButton").innerHTML="Room3";
-  };
+Room3 = () => {
+  let room2 = document.getElementById("dropp3").value;
+  console.log(room2);
+  document.getElementById("room").value = room2;
+  document.getElementById("dropdownMenuButton").innerHTML = "Room3";
+};
