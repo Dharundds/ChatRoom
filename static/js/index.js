f8770251634f37217a43649344da65d71a82a907
Hrithik <hrithikmj003@gmail.com>
2021-04-14 09:00:56 +0530

    added favicon and updated signin.css

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+
+  selectRoom = () => {
+    let room = document.getElementById("dropp").value;
+    console.log(room);
+    document.getElementById("room").value = room;
+    document.getElementById("dropdownMenuButton").innerHTML="Admin";
+  };
+  Room2 = () => {
+    let room2 = document.getElementById("dropp2").value;
+    console.log(room2);
+    document.getElementById("room").value = room2;
+    document.getElementById("dropdownMenuButton").innerHTML="Room2";
+  };
+
+  Room3 = () => {
+    let room2 = document.getElementById("dropp3").value;
+    console.log(room2);
+    document.getElementById("room").value = room2;
+    document.getElementById("dropdownMenuButton").innerHTML="Room3";
+  };
